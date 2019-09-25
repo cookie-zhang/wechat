8197fdd9fe7bdb19a36f9bffd4b0ef2a9ce5758d
zjf <zhangjianfei.com>
2019-09-25 16:04:09 +0800

    \

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,167 @@
-//index.js
-//获取应用实例
-const app = getApp()
+// Page({
+//   data: {
+//     imgUrls: [
+//       'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+//       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+//       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+//     ],
+//     indicatorDots: false,
+//     autoplay: false,
+//     interval: 5000,
+//     duration: 1000
+//   },
+//   changeIndicatorDots: function (e) {
+//     this.setData({
+//       indicatorDots: !this.data.indicatorDots
+//     })
+//   },
+//   changeAutoplay: function (e) {
+//     this.setData({
+//       autoplay: !this.data.autoplay
+//     })
+//   },
+//   intervalChange: function (e) {
+//     this.setData({
+//       interval: e.detail.value
+//     })
+//   },
+//   durationChange: function (e) {
+//     this.setData({
+//       duration: e.detail.value
+//     })
+//   }
+// })
+// Page({
+//   onReady: function (e) {
+//     // 使用 wx.createAudioContext 获取 audio 上下文 context
+//     this.audioCtx = wx.createAudioContext('myAudio')
+//   },
+//   data: {
+//     poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+//     name: '此时此刻',
+//     author: '许巍',
+//     src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+//   },
+//   audioPlay: function () {
+//     this.audioCtx.play()
+//   },
+//   audioPause: function () {
+//     this.audioCtx.pause()
+//   },
+//   audio14: function () {
+//     this.audioCtx.seek(14)
+//   },
+//   audioStart: function () {
+//     this.audioCtx.seek(0)
+//   }
+// })
 
+// Page({
+//   takePhoto() {
+//     const ctx = wx.createCameraContext()
+//     ctx.takePhoto({
+//       quality: 'high',
+//       success: (res) => {
+//         this.setData({
+//           src: res.tempImagePath
+//         })
+//       }
+//     })
+//   },
+//   error(e) {
+//     console.log(e.detail)
+//   }
+// })
+// Page({
+//   formSubmit: function (e) {
+//     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+//   },
+//   formReset: function () {
+//     console.log('form发生了reset事件')
+//   }
+// })
+
+// Page({
+//   data: {
+//     nodes: [{
+//       name: 'div',
+//       attrs: {
+//         class: 'div_class',
+//         style: 'line-height: 60px; color: red;'
+//       },
+//       children: [{
+//         type: 'text',
+//         text: 'hahahhahah!'
+//       }]
+//     }]
+//   },
+//   tap() {
+//     console.log('tap')
+//   },
+//   start(){
+//     console.log('start')
+//   },
+//   move(){
+//     console.log('move')
+//   },
+//   end(){
+//     console.log('end')
+//   },
+//   cacel(){
+//     console.log('cacel')
+//   }
+// })
+// Page({
+//   onReady() {
+//     const query = wx.createSelectorQuery()
+//     query.select('#myCanvas').node().exec((res) => {
+//       const canvas = res[0].node
+//       const gl = canvas.getContext('webgl')
+//       console.log(gl)
+//     })
+//   }
+// })
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    markers: [{
+      iconPath: "/resources/others.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/resources/location.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  regionchange(e) {
+    console.log(e.type)
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  markertap(e) {
+    console.log(e.markerId)
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  controltap(e) {
+    console.log(e.controlId)
   }
 })
